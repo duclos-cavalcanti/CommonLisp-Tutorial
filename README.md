9f7fa0bde36449eaf9a1c1f035f4f54267a1a3b4
Daniel Duclos-Cavalcanti <daniel.duclos.cavalcanti@gmail.com>
2022-02-07 13:41:37 +0100

    Update, WIP

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 Based upon this great [repo](https://github.com/anilsathyan7/lisp). This is just a learning
 experience on `lisp` through the common lisp language.
 
+## Dependencies
+```sh
+sudo pacman -S clisp
+```
+
 ## Acknowledgments
 
 * "Common Lisp In a Nutshell,19th September,2003"
--- math.lisp
+++ math.lisp
@@ -4,6 +4,9 @@
 (defun add(x y)
     (+ x y))
 
+(defun div(x y)
+    (/ x y))
+
 (defun mul(x y)
     (* x y))
 
@@ -11,6 +14,31 @@
     (* x x))
 
 (defun cube(x)
-    (* x (sqr x)))
+    (* x x x ))
+
+(defun recMul(a b)
+    (if (= b 1)
+         a 
+         (+ a (recMul a (- b 1)))))
+
+(defun recPow(a b)
+    (if (= b 1)
+         a 
+         (* a (recPow a (- b 1)))
+    )
+)
+
+(defun fact(n)
+    (if (= n 0)
+        1
+        (* n (fact(- n 1))))
+)
 
 (print (add 12 (cube 10))); prints 1012
+
+(print (recMul 2 3))
+
+(print (recPow 2 3))
+
+(trace fact)
+(fact 4)
