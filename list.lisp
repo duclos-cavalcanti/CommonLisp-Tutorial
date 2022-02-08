b4334073728b3a10a9ba4e69752532fb01c46393
Daniel Duclos-Cavalcanti <daniel.duclos.cavalcanti@gmail.com>
2022-02-08 02:52:12 +0100

    Update

--- list.lisp
+++ list.lisp
@@ -21,14 +21,27 @@
         (+ 1 (_count a (cdr l)))
         (+ 0 (_count a (cdr l))))))
 
+(defun _pos (a l c)
+  (if (null l)
+    nil
+    (if (= a (car l))
+     c 
+     (_pos a (cdr l) (+ c 1)))))
+
+(defun findPos (a l)
+  (_pos a l 0))
+
 ; (trace rev)
+(setq l (list 1 2 2 4 3))
 
-(setq l (list 1 2 2 3 4))
-
-(print (_reverse l))
+(print l)
 (print (_length l))
+(print (_reverse l))
 (print (cdr l))
 (print (car l))
 (print (cons 9 l))
 (print (_append l (list 1 2)))
 (print (_count 2 l))
+
+(trace _pos)
+(print (findPos 4 l))
